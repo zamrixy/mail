3ed07e3c43b8ff22bd60019f00bd45bc18b1c0b8
Marsel Zakirov <100857409+zamrixy@users.noreply.github.com>
2023-08-03 10:02:15 +0500

    Create fip_compute_1.tf

---
+++ fip_compute_1.tf
@@ -0,0 +1,12 @@
+resource "vkcs_networking_floatingip" "fip" {
+  pool = data.vkcs_networking_network.extnet.name
+}
+
+resource "vkcs_compute_floatingip_associate" "fip" {
+  floating_ip = vkcs_networking_floatingip.fip.address
+  instance_id = vkcs_compute_instance.compute_1.id
+}
+
+output "instance_fip" {
+  value = vkcs_networking_floatingip.fip.address
+}
